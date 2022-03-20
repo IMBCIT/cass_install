da74b2388b647c5c2af22e021c34dbda3c74fa09
IMBCIT <42709847+IMBCIT@users.noreply.github.com>
2022-03-20 14:43:28 -0900

    Updating for RPi

--- install.sh
+++ install.sh
@@ -7,20 +7,23 @@ PACKAGES="default-jdk"
 
 case $kernel in
 	Linux)
-		echo "OS: $Linux\nArch: ${arch}\n"
+		echo "OS: Linux\nArch: ${arch}\n"
 		sudo apt update && sudo apt upgrade -y
 		sudo apt install $PACKAGES -y
+		mkdir ~/Cassandra
 		pip install cassandra-driver
-		if ["${arch}" = "armv7l"]; then
+		if [ "${arch}" = "armv7l" ]; then
+			
+			echo "Downloading Cassandra tarball"
 			curl -OL https://dlcdn.apache.org/cassandra/4.0.3/apache-cassandra-4.0.3-bin.tar.gz
-			mv Downloads/apache-cassandra-4.0.3.bin.tar.gz ./
 			tar xzvf apache-cassandra-4.0.3-bin.tar.gz
-			cd apache-cassandra-4.0.3/ && bin/cassandra
+			
+			mv apache-cassandra-4.0.3 ~/Cassandra
+			cd ~/Cassandra/apache-cassandra-4.0.3/ && bin/cassandra
 		else
 			echo "Arch not currently targeted"
 			echo "Exiting..."
 			exit 1
-			;;
 		fi
 		;;
 		
