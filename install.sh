4fbd3b92b60114cbbd394b492a434fdaa0116d79
IMBCIT <42709847+IMBCIT@users.noreply.github.com>
2022-04-26 16:17:33 -0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ case $kernel in
 			sudo apt update
 			sudo apt install cassandra
 			
-		elif [ "${arch}" = "x86_64" ]: then
+		elif [ "${arch}" = "x86_64" ]; then
 		
 			echo "Adding Cassandra Debian packages"
 			echo "deb https://downloads.apache.org/cassandra/debian 40x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
